e40ccd6e97cba41f82c530c63424d67d274a62f1
jimikumar-chinmayfinlease <jimikumar.chhatrala@chinmayfinlease.com>
2024-09-03 17:55:27 +0530

    CRUD-NestJS - Project Setup

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+declare const module: any;
+
+async function bootstrap() {
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(3000);
+
+  if(module.hot){
+
+    module.hot.accept();
+  }
+}
+bootstrap();
